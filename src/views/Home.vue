f00c92d8265e1d5e208609129c698f4bd97cf6ff
vanderrich <hendycoc8888@gmail.com>
2022-07-03 15:45:51 +0700

    first baby steps

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div class="centerChilds">
+    <div id="home">
+        <img width="200" length="200" src="../assets/baked potat.png" class="pfp">
+        <h1>vanderrich#9982</h1>
+        <p>Your local Indonesian nerd/idiot</p>
+        <div id="socialMedias" class="centerChilds">
+            <a class="socialMedia socialMedia-discord" href="https://discordapp.com/users/709950767670493275"></a>
+            <a class="socialMedia socialMedia-github" href="https://github.com/vanderrich"></a>
+            <a class="socialMedia socialMedia-twitter" href="https://twitter.com/vanderrich1"></a>
+        </div>
+        <p>why are you stalking me</p>
+    </div>
+    </div>
+</template>
+<style src="../assets/base.css"></style>
