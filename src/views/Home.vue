908c882b7a0b64335b925856120f9a2907cfbbd1
vanderrich <hendycoc8888@gmail.com>
2022-07-03 20:27:26 +0700

    stuff ig

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
+<script lang="ts">
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
     <div class="centerChilds">
     <div id="home">
-        <img width="200" length="200" src="../assets/baked potat.png" class="pfp">
+        <img width="200" length="200" src="./baked potat.png" class="pfp">
         <h1>vanderrich#9982</h1>
         <p>Your local Indonesian nerd/idiot</p>
         <div id="socialMedias" class="centerChilds">
@@ -10,7 +13,12 @@
             <a class="socialMedia socialMedia-github" href="https://github.com/vanderrich"></a>
             <a class="socialMedia socialMedia-twitter" href="https://twitter.com/vanderrich1"></a>
         </div>
-        <p>why are you stalking me</p>
+        <div id="things" class="centerChilds">
+            <router-link to="/projects">Projects</router-link>
+            <router-link to="/tools">Tools and Languages</router-link>
+        </div>
+        <div class="thicc" />
+        <p>stop stalking me</p>
     </div>
     </div>
 </template>
