25d33962d6a77e338f4e9be30c379343be097fdc
vanderrich <hendycoc8888@gmail.com>
2022-07-04 19:32:45 +0700

    e

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -16,7 +16,7 @@ export default {
     <!-- <p v-if="loading">Loading...</p> -->
     <a class="project" :id="projectNameFormatted" :href="projectLink">
         <figure>
-            <img :src="`../src/assets/images/${projectNameFormatted}.png`" height="200" width="200">
+            <img :src="`@/assets/images/${projectNameFormatted}.png`" height="200" width="200">
             <figcaption>{{ projectName }}</figcaption>
         </figure>
     </a>
