bfb36bf875138aa9429ef0a5a078308c9f8cc470
vanderrich <67942051+vanderrich@users.noreply.github.com>
2022-12-06 14:47:20 +0700

    e

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,12 +10,13 @@ export default function Home() {
             </Head>
             <div class="centerChilds">
                 <div id="home">
-                    <img width="200" src="images\bakedpotat.png" class="pfp" />
+                    <img width="200" src="images\bakedpotat.png" class="pfp" alt="insert funni img here" />
                     <h1>vanderrich#9982</h1>
                     <div id="socialMedias" class="centerChilds">
                         <a class="socialMedia socialMedia-discord" href="https://discordapp.com/users/709950767670493275"></a>
                         <a class="socialMedia socialMedia-github" href="https://github.com/vanderrich"></a>
-                        <a class="socialMedia socialMedia-twitter" href="https://twitter.com/vanderrich1"></a>
+                        <a class="socialMedia socialMedia-wca" href="https://www.worldcubeassociation.org/"></a>
+                        <a class="socialMedia socialMedia-twitter" href="https://youtu.be/cnVaL11hW4g"></a>
                     </div>
                     <div id="things" class="centerChilds">
                         <a href="/projects">Projects</a>
