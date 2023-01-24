f6e7d7843e78bead3c0fc82173a879854941f2c7
vanderrich <67942051+vanderrich@users.noreply.github.com>
2023-01-24 11:51:21 +0700

    added more links

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,9 +11,10 @@ export default () => (
                 <h1>vanderrich#9982</h1>
                 <div id="socialMedias" class="centerChilds">
                     <a class="socialMedia socialMedia-discord" href="https://discordapp.com/users/709950767670493275"></a>
-                    <a class="socialMedia socialMedia-github" href="https://github.com/vanderrich"></a>
                     <a class="socialMedia socialMedia-twitter" href="/twitter"></a>
                     <a class="socialMedia socialMedia-of" href="/onlyfans"></a>
+                    <a class="socialMedia socialMedia-github" href="https://github.com/vanderrich"></a>
+                    <a class="socialMedia socialMedia-stackoverflow" href="https://stackoverflow.com/users/14170200/vanderrich"></a>
                 </div>
                 <div id="things" class="centerChilds">
                     <a href="/projects">Projects</a>
