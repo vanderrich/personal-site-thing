de106db0488d4ad7d0c7dc4f5ac92f0ae6268fa8
vanderrich <67942051+vanderrich@users.noreply.github.com>
2023-01-24 11:10:12 +0700

    added some links

--- routes/onlyfans.tsx
+++ routes/onlyfans.tsx
@@ -1,3 +1,3 @@
 export default () => {
-    return <iframe width="560" height="315" src="https://www.youtube.com/embed/pROjzb-ZoCA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    return <iframe style="margin:auto; width:75vw; height: 75vh" src="https://www.youtube.com/embed/pROjzb-ZoCA?&autoplay=1" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe >
 }
--- routes/twitter.tsx
+++ routes/twitter.tsx
@@ -1,3 +1,3 @@
 export default () => {
-    return <iframe width="560" height="315" src="https://www.youtube.com/embed/cnVaL11hW4g" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    return <iframe style="margin:auto; width:75vw; height: 75vh" src="https://www.youtube.com/embed/cnVaL11hW4g" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
 }
